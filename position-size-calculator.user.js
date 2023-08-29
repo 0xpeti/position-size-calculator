94a6b2d3c8f0207c0330230c0c5a0f5ef35ba19e
0xpeti <0xpeti@gmail.com>
2023-08-29 19:31:57 +0200

    added readme link

--- position-size-calculator.user.js
+++ position-size-calculator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trading Position Size Calculator
 // @namespace    http://tampermonkey.net/
-// @version      1.12
+// @version      1.13
 // @description  Add a position size calculator bar to an exchange
 // @author       0xpeti
 // @match        https://www.bybit.com/*
@@ -13,27 +13,12 @@
 // ==/UserScript==
 
 /* 
-README
-What it does:
-- Adds a handy position size calculator bar to the website of Bybit and Binance trading platforms.
-- Input: Trading Account Size, Risk %, Entry, Stop-Loss, Take Profit.
-- Output: real-time calculations for Position Size, Position Size at current Market Price, Risk, Reward, and Risk:Reward ratio.
-- Colors change based on the type of position (long or short).
 
-How to Set Up:
-1. Install Tampermonkey or a similar userscript manager in your browser.
-2. Add this script to Tampermonkey and save.
-3. Navigate to the desired exchange, and the calculator should appear at the top of the page.
-4. Enter your balance, percent risk, entry, stop, and TP values to get real-time calculations.
-5. You can also set your base currency in the script (BASE_CURRENCY), it is set to USD by defaulr.
+README:
+https://github.com/0xpeti/position-size-calculator/blob/main/README.md
 
-How to Add Other Exchanges:
-- Currently set up for Bybit and Binance, but can be adapted for other exchanges. Simply add a @match URL in the script header to match the exchange you want.
-
-What Happens with Cookies:
-- The calculator saves your last entered values in a cookie.
-- When you navigate back to the page (trading pair), your last values will auto-populate. 
 */
+
 
 (function () {
     'use strict';
